66febbbbea95259ccfc58093ead3f64494b74e8f
Ella <72365100+eilla1@users.noreply.github.com>
2021-07-08 06:54:40 -0700

    change twitter card

--- components/head.js
+++ components/head.js
@@ -22,7 +22,7 @@ export default function HeadObject({ children }) {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card"  content="summary_large_image" />
       <meta name="twitter:site" content={twitter} />
       <meta name="twitter:creator" content={twitter} />
       {children}
